6e99375a0a3db5eb4e3b72d7d7eb70cebecb3e97
Erick Wainaina <116720470+Erick-WG@users.noreply.github.com>
2024-03-21 12:39:19 +0300

    added the comments in the courses app

--- courses/url.py
+++ courses/url.py
@@ -5,5 +5,5 @@ urlpatterns = [
 
     # Other project-level URL patterns...
     path('',home_page,),
-    path(),  # Include app1's URL patterns
+    path(),  # Include app1's URL patterns, these urls map to courses views like we did with the home page.
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 def home_page(request):
     return render(request, "courses/home.html")
+
+
+# create more views for your courses app and remember to map them in your courses urls.py file
