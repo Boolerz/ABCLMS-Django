73a58e5a58a303890b55d93ce0a022dc4334bfe2
Erick Wainaina <116720470+Erick-WG@users.noreply.github.com>
2024-03-21 12:47:30 +0300

    Corrected the naming of your urls path, NOTE: always name core files in their plurals ie, views, models, urls and so on

---
+++ students/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+# from .views import you views functions here
+
+urlpatterns = [
+
+    # Remember the convention of url paths, initial points = index page for the app, this means the first url path you build, the first argument should be empty ie...path('', view_function, name='your_url_name')
+    # path(),  # Include app1's URL patterns, these urls map to courses views like we did with the home page.
+]
